7e9706fd8dad6c4964164598fbca5f2d6fb86efc
James Dunne <james.jdunne@gmail.com>
2015-03-13 11:58:27 -0500

    Fetching image list from i.bittwiddlers.org and doing very crude keyword search in titles.

--- web.go
+++ web.go
@@ -4,11 +4,58 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
 //import "github.com/JamesDunne/go-util/base"
 import "github.com/JamesDunne/go-util/web"
+
+type ImageViewModel struct {
+	ID             int64   `json:"id"`
+	Base62ID       string  `json:"base62id"`
+	Title          string  `json:"title"`
+	Kind           string  `json:"kind"`
+	ImageURL       string  `json:"imageURL"`
+	ThumbURL       string  `json:"thumbURL"`
+	Submitter      string  `json:"submitter,omitempty"`
+	CollectionName string  `json:"collectionName,omitempty"`
+	SourceURL      *string `json:"sourceURL,omitempty"`
+	RedirectToID   *int64  `json:"redirectToID,omitempty"`
+	IsClean        bool    `json:"isClean"`
+}
+
+func queryBit() (list []*ImageViewModel, err error) {
+	// Query i.bittwiddlers.org
+	breq, err := http.NewRequest("GET", "https://i.bittwiddlers.org/api/v1/list", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	breq.Header.Set("Authorization", os.Getenv("BIT_AUTH"))
+	brsp, err := http.DefaultClient.Do(breq)
+	if err != nil {
+		log.Printf("ERROR: %s\n", err)
+		return nil, err
+	}
+
+	list = make([]*ImageViewModel, 0, 1000)
+	json.NewDecoder(brsp.Body).Decode(
+		&struct {
+			Result struct {
+				List *[]*ImageViewModel `json:"list"`
+			} `json:"result"`
+		}{
+			Result: struct {
+				List *[]*ImageViewModel `json:"list"`
+			}{
+				List: &list,
+			},
+		},
+	)
+
+	return list, nil
+}
 
 func processRequest(rsp http.ResponseWriter, req *http.Request) *web.Error {
 	if err := req.ParseForm(); err != nil {
@@ -71,14 +118,92 @@ func processRequest(rsp http.ResponseWriter, req *http.Request) *web.Error {
 	}
 
 otherwise:
-	// Echo back text:
+	// Query i.bittwiddlers.org for the list of images:
+	list, err := queryBit()
+	if err != nil {
+		log.Printf("ERROR: %s\n", err)
+		return nil
+	}
+
+	// Search by keyword:
+	keywords := strings.FieldsFunc(
+		strings.ToLower(text),
+		func(c rune) bool { return strings.ContainsRune(" \n\t:,;.[]!()$%^&*/<>'\"", c) },
+	)
+	log.Printf("%v\n", keywords)
+
+	highest, highest_idx := -1, -1
+	for idx, img := range list {
+		titleLower := strings.ToLower(img.Title)
+
+		words := strings.FieldsFunc(
+			titleLower,
+			func(c rune) bool { return strings.ContainsRune(" \n\t:,;.[]!()$%^&*/<>'\"", c) },
+		)
+
+		h := -2
+
+		// Add points for each keyword match:
+		for _, keyword := range keywords {
+			if strings.Contains(titleLower, keyword) {
+				h += 10
+			}
+		}
+
+		if h > -2 {
+			// Subtract points based on how many extra words there are:
+			h += (len(keywords) - len(words))
+			if h < 0 {
+				h = 0
+			}
+
+			log.Printf("  %4d %s\n", h, titleLower)
+		}
+
+		if h > highest {
+			highest = h
+			highest_idx = idx
+		}
+	}
+
+	if highest_idx == -1 {
+		log.Printf("No match!\n")
+		return nil
+	}
+
+	log.Printf("HIT: %d, %d\n", highest_idx, highest)
+	img := list[highest_idx]
+
+	// Write JSON response with attached image:
 	rsp.Header().Set("Content-Type", "application/json")
 	rsp.WriteHeader(http.StatusOK)
-	json.NewEncoder(rsp).Encode(struct {
-		Text string `json:"text"`
+	o, err := json.Marshal(struct {
+		Text        string `json:"text"`
+		Attachments []struct {
+			Fallback string `json:"fallback"`
+			ImageURL string `json:"image_url"`
+		} `json:"attachments"`
 	}{
-		Text: text,
+		Text: img.Title,
+		Attachments: []struct {
+			Fallback string `json:"fallback"`
+			ImageURL string `json:"image_url"`
+		}{
+			struct {
+				Fallback string `json:"fallback"`
+				ImageURL string `json:"image_url"`
+			}{
+				Fallback: img.Title,
+				ImageURL: "http://i.bittwiddlers.org" + img.ImageURL,
+			},
+		},
 	})
+	log.Printf("%s\n", string(o))
+	if err != nil {
+		log.Printf("ERROR: %s\n", err)
+		return nil
+	}
+	rsp.Write(o)
 
 	return nil
 }
