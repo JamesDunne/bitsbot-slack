523d2cd243fc5fc1b243677f770de99ea9ccab93
James Dunne <james.jdunne@gmail.com>
2015-03-18 10:55:38 -0500

    Test search with no keywords.

--- search_test.go
+++ search_test.go
@@ -16,6 +16,24 @@ func TestBitSearch(t *testing.T) {
 		t.Error(err)
 		return
 	}
+	log.Printf("%d matches\n", len(list))
+	for _, img := range list {
+		log.Printf("%s\n", img.Title)
+	}
+}
+
+func TestBitSearchAll(t *testing.T) {
+	err := parseEnv([]string{"BIT_AUTH"})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	list, err := searchBit([]string{})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	log.Printf("%d matches\n", len(list))
 	for _, img := range list {
 		log.Printf("%s\n", img.Title)
 	}
